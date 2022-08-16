cec3ce3bcb9b347432ecf0e4e5efa1602dad14b4
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2022-08-16 13:41:12 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,33 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
 
-
-    #Parte 1 - Criando um BLOCKCHAIN
 class Blockchain:
-    def  __init__(self):
+
+    def __init__(self):
         self.chain = []
-        self.create_block(proof = 1,previous_hash='0')
-        
-    def create_block(self,proof, previous_hash):
+        self.create_block(proof = 1, previous_hash = '0')
+
+    def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
-                 'proof':proof,
+                 'proof': proof,
                  'previous_hash': previous_hash}
         self.chain.append(block)
         return block
 
     def get_previous_block(self):
         return self.chain[-1]
-    
+
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
@@ -37,12 +29,13 @@ class Blockchain:
                 check_proof = True
             else:
                 new_proof += 1
-            return new_proof
-    def hash(self,block):
-        encoded_block = json.dumps(block,sort_keys=True).encode()
+        return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-     #Fazendo uma função que valida se cada bloco tem o proof of work valido e se cada hash anterior está correto ou seja está lincado com o hash atual de forma correta
-    def is_chain_valid(self,chain):
+    
+    def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
         while block_index < len(chain):
@@ -56,20 +49,43 @@ class Blockchain:
                 return False
             previous_block = block
             block_index += 1
-        return True      
+        return True
 
-
-
-    
 
 app = Flask(__name__)
 
+
 blockchain = Blockchain()
 
-@app.route('/mine_block',methods = ['GET'])
 
+@app.route('/mine_block', methods = ['GET'])
 def mine_block():
-    previous_block = blockchain.get_previous_block() 
+    previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Parabens voce acabou de minerar um bloco!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message' : ' Tudo certo, o blockchain e valido '}
+    else:
+        response = {'message' : ' O blockchain nao e valido '}
+    return jsonify(response), 200
+
+app.run(host = '0.0.0.0', port = 5000)
